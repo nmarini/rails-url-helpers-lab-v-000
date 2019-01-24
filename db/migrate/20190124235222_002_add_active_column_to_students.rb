253056d588db512c6a8c0dc1e02f7989a0e78d6c
Nicolas Marini <nmarini44@gmail.com>
2019-01-24 23:56:38 +0000

    Automatically backed up by Learn

--- db/migrate/20190124235222_002_add_active_column_to_students.rb
+++ db/migrate/20190124235222_002_add_active_column_to_students.rb
@@ -1,4 +1,4 @@
-class AddActiveColumnToStudents < ActiveRecord::Migration
+class 002AddActiveColumnToStudents < ActiveRecord::Migration
   def change
     add_column :students, :active, :boolean, default: false
   end
